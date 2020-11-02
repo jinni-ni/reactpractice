fed94c5c3e31e45bdecf9da5d6e5f79d73d44ae6
김수진 <sjkim910115@gmail.com>
2020-11-03 00:16:03 +0900

    Practice 01

---
+++ src/ThemedButton.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+function ThemedButton({ theme, label, ...restProps }) {
+   return (
+      <div>
+         <button className={`btn btn-${theme}`} {...restProps}>
+            {label}
+         </button>
+      </div>
+   );
+}
+
+ThemedButton.defaultProps = {
+   theme: 'default',
+};
+
+ThemedButton.protoTypes = {
+   theme: PropTypes.string,
+   label: PropTypes.string.isRequired,
+};
+
+export default ThemedButton;
